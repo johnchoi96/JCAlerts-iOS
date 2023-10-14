c2a443c612517f6ef27e0aa1a2002441140cf82d
John Choi <johnchoi1003@icloud.com>
2023-10-14 04:28:42 -0400

    AppDelegate FCM configured

---
+++ JCAlerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JCAlerts
+//
+//  Created by John Choi on 10/7/23.
+//
+
+import UIKit
+import FirebaseMessaging
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
